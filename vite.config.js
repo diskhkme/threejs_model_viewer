289f932c6b8e8b8ebe760e16fd767528d5ea984c
Hyungki Kim <diskhkme@gmail.com>
2025-03-04 11:30:05 +0900

    feat: local test environment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import topLevelAwait from 'vite-plugin-top-level-await'
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
+// --- Deploy Config ---
+// export default defineConfig({
+//   plugins: [topLevelAwait()],
+//   base: './',
+// })
+
+// --- Local Config ---
 export default defineConfig({
-  plugins: [topLevelAwait()],
-  base: './',
-})
+  server: {
+    port: 3000,
+    open: "/test.html", // 개발 서버 시작시 테스트 페이지 자동 오픈
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "/index.html",
+        test: "/test.html",
+      },
+    },
+  },
+});
